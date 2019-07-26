f29d8aa23ea74b06f4b806160e47f231e6936033
Cole Pedersen <colep@lendio.com>
2019-07-25 21:16:42 -0600

    Completed app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  todoArray = [];
+  todo;
+
+  addTodo(value) {
+    if (value !== "") {
+      this.todoArray.push(value)
+    } else {
+      alert('Field required **')
+    }
+  }
+
+  /*delete item*/
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1)
+      }
+    }
+  }
+
+  // submit Form
+  todoSubmit(value: any) {
+    if (value !== "") {
+      this.todoArray.push(value.todo)
+      //this.todoForm.reset()
+    } else {
+      alert('Field required **')
+    }
+
+  }
+
 }
